9a345f00fdefff21ea85bafb75c96ba6fd80277b
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2023-11-21 00:58:24 +0100

    updates

--- custom_command/copy_path_pdf2docx.bat
+++ custom_command/copy_path_pdf2docx.bat
@@ -2,4 +2,4 @@
 
 set "repo_dir_pdf=%cd%"
 echo repo_dir_pdf: %repo_dir_pdf%
-xcopy C:\Users\%username%\pdf2docx_converter\custom_command C:\Users\%username%\xbin_windows /e /y /d
+xcopy %repo_dir_pdf%\custom_command C:\Users\%username%\xbin_windows /e /y /d
