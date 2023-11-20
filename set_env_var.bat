4dcbc4d0769cb68b22a38a719cb3f12ea812fb0c
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2023-11-20 20:15:09 +0100

    updates

--- set_env_var.bat
+++ set_env_var.bat
@@ -54,6 +54,8 @@ for /f "tokens=2,*" %%I in ('reg query "HKCU\Environment" /v PATH ^| find "REG_S
 )
 rem echo UPDATED_ENV_VAR: %UPDATED_ENV_VAR_PATH%
 rem echo ....................................................................
-
+call .custom_command\copy_path_pdf2docx.bat
+echo .
+echo .
 echo We rise by lifting others.
 endlocal
