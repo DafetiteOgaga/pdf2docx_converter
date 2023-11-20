7ea99664d25d49ee032db1cb9d5e24f1956c295f
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2023-11-20 01:03:25 +0100

    bug fix

--- pdf2docx_installer.bat
+++ pdf2docx_installer.bat
@@ -1,6 +1,6 @@
 @echo off
 
-echo "Installing PDF to DOCX Converter ..."
+echo Installing PDF to DOCX Converter ...
 set "wrong_dir=%cd%"
 
 set "user_dir=C:\Users\%username%"
@@ -14,11 +14,11 @@ if exist "%user_dir%\pdf2docx_converter" (
 
 set "pdf2docx_converter=%user_dir%\pdf2docx_converter"
 
-echo "Setting up neccessary files ..."
+echo Setting up neccessary files ...
 cd %pdf2docx_converter%
 call set_env_var.bat
 
-echo "Finishing setup ..."
+echo Finishing setup ...
 cd %pdf2docx_converter%
 xcopy %pdf2docx_converter%\.custom_command C:\Users\%username%\xbin_windows /e /y
 
