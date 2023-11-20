5c29223f0b8d9376faaa9c40e174836e22283ff6
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2023-11-21 00:30:19 +0100

    updates

--- .custom_command/copy_path_pdf2docx.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-xcopy C:\Users\%username%\pdf2docx_converter\.custom_command C:\Users\%username%\xbin_windows /e /y /d
--- pdf2docx_installer_1.bat
+++ pdf2docx_installer_1.bat
@@ -16,7 +16,7 @@ call pdf2docx_installer_2.bat
 set /P "resp=Do you want to run the program now? [y/N] >>> "
 
 if /i "%resp%"=="y" if not "%resp%"=="" (
-	call "%dir%\.custom_command\pdf2word.bat"
+	call "%dir%\custom_command\pdf2word.bat"
 ) else (
 	echo .
 	echo To run the program, run: runserver_all
