1b082a93202466e77149a88ae8602a2cb2ee1b6e
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2023-11-20 01:18:30 +0100

    bug fix

--- pdf2docx_installer.bat
+++ pdf2docx_installer.bat
@@ -32,10 +32,9 @@ if "%user_dir%" equ "%wrong_dir%" (
 ) else (
 	echo Cleaning ...
     cd %wrong_dir%
-	echo %cd%
 	cd ..
-	echo %cd%
 	rmdir /s /q pdf2docx_converter
+	echo %cd%
 )
 
 echo Installation complete.
