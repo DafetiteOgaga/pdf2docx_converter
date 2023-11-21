e4a59d60634b9f46c7268714fd8f594ce40b3711
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2023-11-21 10:45:05 +0100

    updates

--- pdf2docx_installer_1.bat
+++ pdf2docx_installer_1.bat
@@ -8,13 +8,5 @@ set "dir=C:\Users\%username%\pdf2docx_converter"
 
 call pdf2docx_installer_2.bat
 
-set /P "resp=Do you want to run the program now? [y/N] >>> "
-
-if /i "%resp%"=="y" if not "%resp%"=="" (
-	call "%dir%\custom_command\pdf2word.bat"
-) else (
-	echo .
-	echo To run the program, run: runserver_all
-	echo .
-    exit /b 0
-)
+echo Now, close this window.
+echo Open another window and run: pdf2word
